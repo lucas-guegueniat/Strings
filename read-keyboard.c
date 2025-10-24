9dd38206ff6dd2a797d4f9e8ac3b2c11f3fae5f5
Lucas GUEGUENIAT <lucas.guegueniat@edu.esiee.fr>
2025-10-24 08:11:23 +0000

    Lecture clavier

--- read-keyboard.c
+++ read-keyboard.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void caracteres(char s[]){
-    char *p = s;
-    while (*p != '\0'){
-        printf("Caractère %c : %c", p, *p);
-        p++;
-    }
+//// Pour des mots ////
+
+// void caracteres(char s[]){
+    // char *p = s;
+    // int i = 0;
+    // while (*p != '\0'){
+        // printf("Caractère %3d : %c\n", i, *p);
+        // p++;
+        // i++;
+    // }
+// }
+
+// int main(){
+    // char s[100];
+// 
+    // printf("Entrez une chaine de caractères : %s\n", s);
+    // fgets(s, 100, stdin);
+    // printf("La chaine de caractères récupérée dans s : %s\n", s);
+    // printf("composée de %ld caractères lus au clavier\n", strlen(s));
+    // caracteres(s);
+// }
+
+
+//// Pour des nombres ////
+
+void nombres(char s[]){
+    int i = atoi(s);
+    printf("Après conversion : %d\n", i);
+    printf("et multiplication par 2 : %d\n", i*2);
 }
 
 int main(){
@@ -14,7 +38,5 @@ int main(){
 
     printf("Entrez une chaine de caractères : %s\n", s);
     fgets(s, 100, stdin);
-    printf("La chaine de caractères récupérée dans s : %s\n", s);
-    printf("composée de %ld caractères lus au clavier\n", strlen(s));
-    caracteres(s);
+    nombres(s);
 }
